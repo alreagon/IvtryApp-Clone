895e9624d06bb9c55c77592121a2dfdbe8e4080f
alreagon <alpharajasa@gmail.com>
2023-02-28 14:31:33 +0700

    Commit check

---
+++ app/src/main/java/com/zexceed/inventoryarjanpdlkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zexceed.inventoryarjanpdlkotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class MainActivity : AppCompatActivity() {
+}
